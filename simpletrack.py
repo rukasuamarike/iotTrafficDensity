5bed9a95542466959eef82464403f334f3340c2a
rukasuamarike <lukeamlicke@gmail.com>
2024-06-08 10:05:11 -0700

    e

---
+++ simpletrack.py
@@ -0,0 +1,36 @@
+import cv2
+from ultralytics import YOLO
+from ultralytics import YOLOv10
+import supervision as sv
+
+# Load the YOLOv8 model
+# model = YOLO("yolov10n.pt")
+model = YOLOv10(f"weights/yolov10n.pt")
+
+# Open the video file
+video_path = "path/to/video.mp4"
+cap = cv2.VideoCapture(1)
+# Loop through the video frames
+while True:
+    # Read a frame from the video
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Can't receive frame (stream end?). Exiting...")
+        break
+
+    # Run YOLOv8 tracking on the frame, persisting tracks between frames
+    results = model.track(frame, persist=True)
+
+    # Visualize the results on the frame
+    annotated_frame = results[0].plot()
+
+    # Display the annotated frame
+    cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
