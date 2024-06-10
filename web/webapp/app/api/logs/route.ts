902eae05d6fa98a6b76c0c54420dc480f1f29b1a
rukasuamarike <lukeamlicke@gmail.com>
2024-06-09 18:56:12 -0700

    no cache

--- web/webapp/app/api/logs/route.ts
+++ web/webapp/app/api/logs/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const vehicleLogsResponse = await fetch('https://fb6c-2601-647-4d83-3930-00-da51.ngrok-free.app/vehicle-logs');
+  const vehicleLogsResponse = await fetch('https://fb6c-2601-647-4d83-3930-00-da51.ngrok-free.app/vehicle-logs',{ cache: 'no-store' });
   const vehicleLogs = await vehicleLogsResponse.json();
   console.log("ASDASD"+vehicleLogs)
   return NextResponse.json({data:vehicleLogs});
--- web/webapp/app/api/stat/route.ts
+++ web/webapp/app/api/stat/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const trafficStatsResponse = await fetch('https://fb6c-2601-647-4d83-3930-00-da51.ngrok-free.app/traffic-stats');
+  const trafficStatsResponse = await fetch('https://fb6c-2601-647-4d83-3930-00-da51.ngrok-free.app/traffic-stats',{ cache: 'no-store' });
   const trafficStats = await trafficStatsResponse.json();
   console.log(trafficStats)
   return NextResponse.json({data:trafficStats});
