0b14e0a9eee3d8f438472e077284c3244eb5cffc
owenmatejka <owenmatejka+github@gmail.com>
2024-06-09 18:21:26 -0700

    Update ngrok

--- web/webapp/app/api/logs/route.ts
+++ web/webapp/app/api/logs/route.ts
@@ -3,7 +3,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const vehicleLogsResponse = await fetch('https://c449-129-210-115-236.ngrok-free.app/vehicle-logs');
+  const vehicleLogsResponse = await fetch('https://fb6c-2601-647-4d83-3930-00-da51.ngrok-free.app/vehicle-logs');
   const vehicleLogs = await vehicleLogsResponse.json();
   console.log("ASDASD"+vehicleLogs)
   return NextResponse.json({data:vehicleLogs});
--- web/webapp/app/api/stat/route.ts
+++ web/webapp/app/api/stat/route.ts
@@ -3,7 +3,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const trafficStatsResponse = await fetch('https://c449-129-210-115-236.ngrok-free.app/traffic-stats');
+  const trafficStatsResponse = await fetch('https://fb6c-2601-647-4d83-3930-00-da51.ngrok-free.app/traffic-stats');
   const trafficStats = await trafficStatsResponse.json();
   console.log(trafficStats)
   return NextResponse.json({data:trafficStats});
